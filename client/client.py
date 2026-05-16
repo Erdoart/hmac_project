d9cf2284d078a1e820d9cb97512131cd0451cf96
ErdaKryeziu <erda.kryeziu@student.uni-pr.edu>
2026-05-17 00:36:35 +0200

    Ndryshime te klienti

--- client/client.py
+++ client/client.py
@@ -3,36 +3,26 @@ import socket
 from client.hmac_utils import generate_hmac
 from shared.config import HOST, PORT
 
-# krijimi i socket-it
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# lidhja me serverin
-client.connect((HOST, PORT))
-
-print("Connected to server!")
-
 while True:
-
-    # input nga useri
     message = input("Write message: ")
 
-    # me dal prej programit
     if message.lower() == "exit":
         break
 
-    # gjenerimi i HMAC
-    generated_hmac = generate_hmac(message)
+    if "|" in message:
+        print("Message cannot contain | character.")
+        continue
 
-    # kombinimi i message + hmac
+    generated_hmac = generate_hmac(message)
     data = f"{message}|{generated_hmac}"
 
-    # dergimi te serveri
-    client.send(data.encode())
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+            client.connect((HOST, PORT))
+            client.sendall(data.encode())
 
-    # marrja e pergjigjes
-    response = client.recv(1024).decode()
+            response = client.recv(1024).decode()
+            print(f"Server response: {response}")
 
-    print(f"Server response: {response}")
-
-# mbyllja e lidhjes
-client.close()
+    except ConnectionRefusedError:
+        print("Could not connect to server. Make sure server.py is running.")
